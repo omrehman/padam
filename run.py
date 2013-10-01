6cc4d30ceb74e012a858146868a77e9b9087a58b
Mujeeb Rehman <mujeeb@okrashe123@gmail.com>
2013-10-01 11:14:47 +0530

    change in run.py

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+#############################################################
+######This is the main file which can be run#################
+############################################################
+
 from flsk_app import app
-#app.run(debug = True,port=2030,host='192.168.16.32')
 app.run(debug = True,port=2030)
